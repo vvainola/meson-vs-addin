11b5eb149b9e644faa59ad94f2a364c4ca0bfcfb
Vili Väinölä <vilivainola@gmail.com>
2020-06-06 09:06:22 +0300

    Add top level meson menu

--- MesonPlugin/MesonPluginPackage.cs
+++ MesonPlugin/MesonPluginPackage.cs
@@ -25,6 +25,7 @@ namespace MesonPlugin
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(MesonPluginPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class MesonPluginPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace MesonPlugin
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await MesonMenu.InitializeAsync(this);
         }
 
         #endregion
